397f05607b326f6da4782d06bfee4c9e8359a9ab
Henry Stoupe <H.stoupe@gmail.com>
2019-11-18 23:33:30 +1300

    JSON planning

--- src/nz/ac/vuw/ecs/swen225/a3/maze/WallTile.java
+++ src/nz/ac/vuw/ecs/swen225/a3/maze/WallTile.java
@@ -7,16 +7,6 @@ public class WallTile extends GenericTile {
     }
 
     @Override
-    public int getX() {
-        return 0;
-    }
-
-    @Override
-    public int getY() {
-        return 0;
-    }
-
-    @Override
     public String toString() {
         return "Wall Tile";
     }
